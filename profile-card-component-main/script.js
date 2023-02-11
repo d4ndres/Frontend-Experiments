1226b5ee3dd764d1f9a688071389e5759fb39143
d4ndres <d4ndres@gmail.com>
2023-02-10 18:57:37 -0600

    some experiments background blend mode

--- profile-card-component-main/script.js
+++ profile-card-component-main/script.js
@@ -10,8 +10,13 @@ fetch('https://randomuser.me/api/')
     <figure >
         <img src="${user.picture.large}" alt="${user.name.title} ${user.name.last}">
     </figure>
-    <h1><Strong>${user.name.first} ${user.name.last}</Strong> <span>${user.dob.age}</span> </h1>
-    <p>${user.location.city}</p>   
+    <div class="base__data__user">
+        <h1><Strong>${user.name.first} ${user.name.last}</Strong> <span>${user.dob.age}</span> </h1>
+        <p>${user.location.city}</p>   
+    </div>
 
     `
+})
+.catch( error => {
+    console.error(error) 
 })
